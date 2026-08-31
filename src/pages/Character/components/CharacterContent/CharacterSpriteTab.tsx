44fd916cbcef7c5737385c21db5c65ccd81e619f
IceProgrammer <13706531210@163.com>
2026-08-31 21:03:50 +0800

    ✨ feat(character): add new outfit creation button and adjust tab padding

--- src/pages/Character/components/CharacterContent/CharacterSpriteTab.tsx
+++ src/pages/Character/components/CharacterContent/CharacterSpriteTab.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Flex } from 'antd';
+import { Button, Flex } from 'antd';
 import { CustomSegmented } from '@/components/CustomSegmented';
+import { PlusOutlined } from '@ant-design/icons';
 
 export const CharacterSpriteTab: React.FC = () => {
   return (
     <Flex vertical align="start">
-      <CustomSegmented
-        width="70%"
-        options={['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']}
-      />
+      <Flex align="center" justify="space-between" style={{ width: '100%' }}>
+        <CustomSegmented
+          width="70%"
+          options={['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']}
+        />
+        <Button type="primary" icon={<PlusOutlined />}>
+          新建套装
+        </Button>
+      </Flex>
     </Flex>
   );
 };
